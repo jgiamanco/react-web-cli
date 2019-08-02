d2fe16e1ef0bed9afc5505186d0866cbe8e76295
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-08-02 21:40:49 +0530

    Configuring front-end

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  fetch('/api').then(res => console.log(res));
   return (
     <div className="App">
       <header className="App-header">
