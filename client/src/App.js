1625d2e9b455a781371d7e4b979a2f5acd308a7e
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-08-03 11:05:53 +0530

    Fetching data from the server to front-end, testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  fetch('/api').then(res => console.log(res));
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  fileSystemHandler = () => {
+    fetch('/fileSystem')
+      .then(res => res.json())
+      .then(data => console.log(data));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <br />
+          <br />
+          <button onClick={() => this.fileSystemHandler()}>
+            Access File System
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
