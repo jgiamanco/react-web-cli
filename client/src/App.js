43aca3020a205a1b06a411a53ab98a4fb64dd05d
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-08-03 13:59:40 +0530

    Configuring front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,72 @@
 import React, { Component } from 'react';
+import Switch from 'react-switch';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  fileSystemHandler = () => {
+  state = {
+    checked: false,
+    cwd: '',
+    packageManager: '',
+    packages: {
+      ReactRouter: false,
+      Redux: false,
+      ReduxThunk: false,
+      Unstated: false,
+      UnstatedNext: false,
+      TypeScript: false,
+      NodeSass: false,
+      StyledComponents: false,
+      types: {
+        Node: false,
+        ReactRouter: false,
+        Redux: false,
+        Jest: false
+      }
+    }
+  };
+
+  getCWD = e => {
+    // const { name, value } = e.target;
     fetch('/fileSystem')
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({ cwd: data.path }));
+  };
+
+  packageCheckHandler = e => {
+    console.log(e);
+    const { name } = e;
+    this.setState({ packages: { [name]: !this.state.packages[name] } });
   };
 
   render() {
+    console.log(this.state.packages);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <label>
+            <h6>React Router</h6>
+            <Switch
+              id="ReactRouter"
+              name="ReactRouter"
+              onChange={() =>
+                this.packageCheckHandler({
+                  name: 'ReactRouter'
+                })
+              }
+              checked={this.state.packages.ReactRouter}
+            />
+          </label>
           <br />
           <br />
-          <button onClick={() => this.fileSystemHandler()}>
-            Access File System
-          </button>
+          <button onClick={() => this.getCWD()}>Access File System</button>
+          {this.state.cwd > '' ? (
+            <h5>
+              Your Current Working Directory is: <br />{' '}
+              <em>{this.state.cwd}</em>
+            </h5>
+          ) : null}
         </header>
       </div>
     );
