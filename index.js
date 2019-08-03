d42a5ae4a25a76adfc9684764efa27234e074ca6
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-08-03 11:05:34 +0530

    Testing some responses

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
+const FileSystem = process.cwd();
+
+console.log(FileSystem);
+
 app.get('/api', (req, res, next) => {
   res.send('Server Connected');
+});
+
+app.get('/fileSystem', (req, res) => {
+  res.status(200).send({ path: `${FileSystem}` });
 });
 
 const PORT = process.env.PORT || 3400;
